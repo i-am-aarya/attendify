203dffb52ef4d4a77bc5e5801ac66736201fba67
arya <aryabhattarai180@gmail.com>
2024-01-05 14:23:42 +0545

    update sidebarexpandable.tsx

--- attendify-frontend/attendify/src/components/sidebar/SidebarExpandable.tsx
+++ attendify-frontend/attendify/src/components/sidebar/SidebarExpandable.tsx
@@ -1,35 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./SidebarExpandable.css";
+import ExpandedOptionsPanel from "./expandedoptions/ExpandedOptionsPanel";
 
-function doSth() {}
 
-const SidebarExpandable = ({text}: { text: string }) => {
-
+const SidebarExpandable = ({ text }: { text: string }) => {
   const [expanded, setExpanded] = useState(false);
 
   const handleExpandableClick = () => {
-    setExpanded(!expanded)
-  }
-
+    setExpanded(!expanded);
+  };
 
   return (
     <>
-      <div className="sidebar-selector">
-        <ul className="sidebar-list">
-          <li className="selector-text">{text}</li>
+      <div
+        className={
+          expanded ? "sidebar-expandable-expanded" : "sidebar-expandable"
+        }
+        onClick={handleExpandableClick}
+      >
+        {
+          expanded ? (
 
-          <li>
-            <svg className="dropdown-icon"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 -960 960 960"
-              width="24">
-              <path
-                d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"
-                fill="#ffffff"/>
-            </svg>
-          </li>
-        </ul>
+
+            <div className="expandable-content">
+            
+              <ul className="sidebar-list">
+                <li className="selector-text">{text}</li>
+                <li>
+
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m296-345-56-56 240-240 240 240-56 56-184-184-184 184Z" fill="#FFFFFFFF"/></svg>
+
+                </li>
+              </ul>
+
+
+              <ExpandedOptionsPanel/>
+
+            </div>
+
+
+          ) : (
+            // display Semesters
+            <ul className="sidebar-list">
+              <li className="selector-text">{text}</li>
+              <li>
+                <svg
+                  className="dropdown-icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path
+                    d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"
+                    fill="#FFFFFF80"
+                  />
+                </svg>
+              </li>
+            </ul>
+          )
+          // display shift name
+        }
       </div>
     </>
   );
