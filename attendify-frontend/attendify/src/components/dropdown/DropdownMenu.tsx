d7d9ca38611d30d79b94ad62e7b8c7b699ca8531
Pocky <blank00154@gmail.com>
2024-01-06 13:59:15 +0545

    Modified css

--- attendify-frontend/attendify/src/components/dropdown/DropdownMenu.tsx
+++ attendify-frontend/attendify/src/components/dropdown/DropdownMenu.tsx
@@ -1,41 +1,16 @@
 import "./DropdownMenu.css"
-import React, { useState } from "react";
 
-// const DropdownMenu = () => {
-function DropdownMenu() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
-
+const DropdownMenu = () => {
   return (
     <div className="dropdown">
-      <div onClick={toggleMenu} className="dropdown-toggle">
-        
         <div className="dropdown-teacher-name">
-          Teacher's Name
+          Name of the Teacher
         </div>
-
-        <div className="dropdown-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z" fill="#ffffff"/></svg>
+        <div className="dropdown-log-out">
+          Log Out
         </div>
-
       </div>
-      {
-        isOpen && (
-            <ul className="dropdown-menu">
-                <li>Log Out</li>
-            </ul>
-        )
-      }
-      {isOpen && <div onClick={closeMenu} className="dropdown-backdrop"></div>}
-    </div>
-  );
+  )
 };
 
 export default DropdownMenu;
