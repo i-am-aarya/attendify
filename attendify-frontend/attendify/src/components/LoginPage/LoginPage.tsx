d03c7e916d351d2d643511d730a4847d5860af58
Pocky <blank00154@gmail.com>
2024-01-15 14:45:40 +0545

    Login functions

--- attendify-frontend/attendify/src/components/LoginPage/LoginPage.tsx
+++ attendify-frontend/attendify/src/components/LoginPage/LoginPage.tsx
@@ -1,106 +1,91 @@
-// import React from 'react'
-// import "./LoginPage.css"
-// import LoginComponent from './LoginComponent/LoginComponent'
+import React, { useState, ChangeEvent } from 'react';
+import './LoginPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-// const LoginPage = () => {
-//   return (
-//     <div className="login-page">
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-//         <div className="left-container">
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    setError('');
+  };
 
-//             <div className="text-container">
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-//                 <div className="title-wrapper">
-//                     Attendify
-//                 </div>
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-//                 <div className="description-wrapper">
-//                     Digitize attendance tracking.
-//                 </div>
+  const handleLoginClick = () => {
+    // Validate if the email ends with "ncit.edu.np"
+    const isGmail = /@ncit.edu\.np$/.test(email);
 
-//             </div>
-//         </div>
+    if (isGmail) {
+      // Perform login logic
+      console.log('Login successful');
+    } else {
+      setError('Please enter a valid college email');
+    }
+  };
 
-//         <div className="right-container">
-//             <div className="overlapping-rectangles-container">
-
-//                 <div className="rectangle-top">
-
-//                     <LoginComponent/>
-
-//                 </div>
-
-//                 <div className="rectangle-bottom"></div>
-
-//             </div>
-//         </div>
-
-
-//     </div>
-//   )
-// }
-
-// export default LoginPage
-
-import React from 'react';
-import './LoginPage.css'; 
-
-const LoginPage = () => {
   return (
     <div className="container">
-
-    <div className="left-container">
-
-        <div className="title-container">
-            Attendify
-
-        </div>
-
+      <div className="left-container">
+        <div className="title-container">Attendify</div>
         <div className="description-container">
-            <p>
-                “ Streamline attendance tracking through digitization. "
-            </p>
-
+          <p>Streamline attendance tracking through digitization.</p>
         </div>
+      </div>
 
-
-    </div>
-
-    <div className="right-container">
-        
+      <div className="right-container">
         <div className="top-rectangle">
-
-            <div className="login-container">
-
-                <div className="login-heading-wrapper">
-
-                   <p>Login</p>
-
-                </div><br/>
-
-                <div className="username-wrapper">
-                   <p> Username</p>
-                    <input type="text" className="username-input-box"/>
-                </div>
-
-
-                <div className="password-wrapper">
-                   <p>Password</p>
-                    <input type="password" name="" id="" className="password-input-box"/>
-                </div><br/><br/>
-
-                <div className="login-button">
-                    <p>Login</p>
-                </div>
-
+          <div className="login-container">
+            <div className="login-heading-wrapper">
+              <p>Login</p>
             </div>
 
+            <div className="username-wrapper">
+              <p>Email</p>
+              <input
+                type="text"
+                className="username-input-box"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              {error && <div className="error-message">{error}</div>}
+            </div>
+            <br />
+
+            <div className="password-wrapper">
+              <p>Password</p>
+              <div className="password-input-container">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className="password-input-box"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <div className="toggle-password" onClick={handleTogglePassword}>
+                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                </div>
+              </div>
+            </div>
+
+            <div className="login-button" onClick={handleLoginClick}>
+              <p>Login</p>
+            </div>
+          </div>
         </div>
 
         <div className="bottom-rectangle"></div>
-
+      </div>
     </div>
-</div>
   );
 };
 
