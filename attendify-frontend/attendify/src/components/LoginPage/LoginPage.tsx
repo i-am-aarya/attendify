19497b4f2eb91dc911ba0f60c301d456f0713c1e
Pocky <blank00154@gmail.com>
2024-01-15 14:13:07 +0545

    Login Page fix up

--- attendify-frontend/attendify/src/components/LoginPage/LoginPage.tsx
+++ attendify-frontend/attendify/src/components/LoginPage/LoginPage.tsx
@@ -1,44 +1,107 @@
-import React from 'react'
-import "./LoginPage.css"
-import "./background-image.png"
-import LoginComponent from './LoginComponent/LoginComponent'
+// import React from 'react'
+// import "./LoginPage.css"
+// import LoginComponent from './LoginComponent/LoginComponent'
+
+// const LoginPage = () => {
+//   return (
+//     <div className="login-page">
+
+//         <div className="left-container">
+
+//             <div className="text-container">
+
+//                 <div className="title-wrapper">
+//                     Attendify
+//                 </div>
+
+//                 <div className="description-wrapper">
+//                     Digitize attendance tracking.
+//                 </div>
+
+//             </div>
+//         </div>
+
+//         <div className="right-container">
+//             <div className="overlapping-rectangles-container">
+
+//                 <div className="rectangle-top">
+
+//                     <LoginComponent/>
+
+//                 </div>
+
+//                 <div className="rectangle-bottom"></div>
+
+//             </div>
+//         </div>
+
+
+//     </div>
+//   )
+// }
+
+// export default LoginPage
+
+import React from 'react';
+import './LoginPage.css'; 
 
 const LoginPage = () => {
   return (
-    <div className="login-page">
+    <div className="container">
 
-        <div className="left-container">
+    <div className="left-container">
 
-            <div className="text-container">
+        <div className="title-container">
+            Attendify
 
-                <div className="title-wrapper">
-                    Attendify
-                </div>
-
-                <div className="description-wrapper">
-                    Digitize attendance tracking.
-                </div>
-
-            </div>
         </div>
 
-        <div className="right-container">
-            <div className="overlapping-rectangles-container">
+        <div className="description-container">
+            <p>
+                “ Streamline attendance tracking through digitization. "
+            </p>
 
-                <div className="rectangle-top">
-
-                    <LoginComponent/>
-
-                </div>
-
-                <div className="rectangle-bottom"></div>
-
-            </div>
         </div>
 
 
     </div>
-  )
-}
 
-export default LoginPage
+    <div className="right-container">
+        
+        <div className="top-rectangle">
+
+            <div className="login-container">
+
+                <div className="login-heading-wrapper">
+
+                   <p>Login</p>
+
+                </div><br/>
+
+                <div className="username-wrapper">
+                   <p> Username</p>
+                    <input type="text" className="username-input-box"/>
+                </div>
+
+
+                <div className="password-wrapper">
+                   <p>Password</p>
+                    <input type="password" name="" id="" className="password-input-box"/>
+                </div><br/><br/>
+
+                <div className="login-button">
+                    <p>Login</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div className="bottom-rectangle"></div>
+
+    </div>
+</div>
+  );
+};
+
+export default LoginPage;
