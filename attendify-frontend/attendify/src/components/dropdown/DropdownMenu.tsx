8fca418af658430052f225ffc8b854065714cee8
arya <aryabhattarai180@gmail.com>
2024-01-18 19:32:58 +0545

    update DropdownMenu inside NavBar. Logout button works

--- attendify-frontend/attendify/src/components/dropdown/DropdownMenu.tsx
+++ attendify-frontend/attendify/src/components/dropdown/DropdownMenu.tsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react";
 import "./DropdownMenu.css"
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from 'react-router-dom'
+
+interface Teacher {
+  exp: BigInteger
+  emailID: string
+}
 
 const DropdownMenu = () => {
+  const navigate = useNavigate()
+
+  const [teacherName, setTeacherName] = useState("")
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode<Teacher>(token)
+      // setTeacherName(decoded['emailID'])
+      setTeacherName(decoded.emailID)
+
+      console.log(decoded)
+    }
+
+  })
+
+  function handleLogout() {
+    localStorage.removeItem('token')
+    navigate('/')
+    setTeacherName("")
+  }
+
   return (
     <div className="dropdown">
         <div className="dropdown-teacher-name">
-          Name of the Teacher
+          {teacherName}
         </div>
-        <div className="dropdown-log-out">
+        <button className="dropdown-log-out" onClick={handleLogout}>
           Log Out
-        </div>
+        </button>
       </div>
   )
 };
